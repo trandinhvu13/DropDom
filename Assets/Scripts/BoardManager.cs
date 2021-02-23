b93277fb5f5e4b9594fe1bdc7169842ddd2d24e9
Trần Đình Vũ <53107088+trandinhvu13@users.noreply.github.com>
2021-02-23 18:08:55 +0700

    alg

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour
 {
@@ -24,6 +26,7 @@ public class BoardManager : MonoBehaviour
         {
             _instance = this;
         }
+
         SetUpBoard();
     }
 
@@ -31,12 +34,26 @@ public class BoardManager : MonoBehaviour
 
     #region Variables
 
-    [SerializeField] private GameObject[,] gridGameObjects = new GameObject[8,10];
+    [SerializeField] private GameObject[,] gridGameObjects = new GameObject[8, 10];
     [SerializeField] private GameObject grid;
 
     #endregion
 
     #region Mono
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateRow();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int test = Random.Range(1, 5);
+            Debug.Log(test);
+        }
+    }
 
     #endregion
 
@@ -50,7 +67,136 @@ public class BoardManager : MonoBehaviour
             Vector2 tempPos = (o = Child.gameObject).GetComponent<Tile>().pos;
             gridGameObjects[(int) tempPos.x, (int) tempPos.y] = o;
         }
+    }
 
+    private void GenerateRow()
+    {
+        //int blankNum = Random.Range(1, 3); //how many blank in a row
+        int blankNum = 1;
+
+        int[] lineValue = {-1, -1, -1, -1, -1, -1, -1, -1}; //array represents upcoming line
+
+        if (blankNum == 1)
+        {
+            int blankPos = Random.Range(1, 8 + 1); //position of the blank
+            //int blankPos = 2;
+            int blankBlockLength = Random.Range(1, 8 - blankPos + 1); //length of that blank
+
+            Debug.Log("Blank Pos: " + blankPos);
+
+            while (blankBlockLength > 4)
+            {
+                blankBlockLength = Random.Range(1, 8 - blankPos + 1);
+            }
+
+            var numOfUnassignedTiles = 8 - blankBlockLength;
+            for (int i = 0; i < blankBlockLength; i++)
+            {
+                lineValue[blankPos - 1 + i] = 0;
+            }
+
+            Debug.Log("Blank Length: " + blankBlockLength);
+            DebugLogArray(lineValue);
+            int numOfBlocksInARow =
+                Random.Range(1, 8 - blankBlockLength + 1); //number of separate blocks in a row (exclude blank)
+            while ((numOfUnassignedTiles / numOfBlocksInARow) > 4 || (numOfUnassignedTiles / numOfBlocksInARow) < 1 ||
+                   numOfBlocksInARow > 4)
+            {
+                Debug.Log("Again: Unassigned: " + numOfUnassignedTiles + "| num of block+ " + numOfBlocksInARow);
+                numOfBlocksInARow = Random.Range(2, 8 - blankBlockLength + 1);
+                while (numOfBlocksInARow == 2 || blankPos == 1 || blankPos == 2 || blankPos == 7 || blankPos == 8)
+                {
+                    numOfBlocksInARow = Random.Range(2, 8 - blankBlockLength + 1);
+                }
+            }
+
+            lineValue[blankPos - 1] = 0; //0 = blank, -1 = unassigned value
+            if (blankPos == 1 || blankPos == 8)
+            {
+                GenerateBlock(numOfUnassignedTiles, numOfBlocksInARow);
+            }
+            else
+            {
+                int numOfBlocksInARow1 = Random.Range(1, blankPos - 1 + 1);
+                GenerateBlock(blankPos - 1, numOfBlocksInARow1); //generate for the first blank part
+
+                Debug.Log("Second part---------------------------------");
+                int numOfRemainingTiles = 8 - (blankPos + blankBlockLength - 1);
+
+                int numOfBlocksInARow2 = Random.Range(1, numOfBlocksInARow - numOfBlocksInARow1 + 1);
+                while (numOfBlocksInARow2 > 4)
+                {
+                    numOfBlocksInARow2 = Random.Range(1, numOfBlocksInARow - numOfBlocksInARow1 + 1);
+                }
+                GenerateBlock(numOfRemainingTiles, numOfBlocksInARow2); //generate for the 2nd blank part
+            }
+        }
+        else //rare
+        {
+        }
+
+        void GenerateBlock(int _unassignedTileNum, int _blocksInARowNum)
+        {
+            int unassignedTileNum = _unassignedTileNum;
+            int blocksInARowNum = _blocksInARowNum;
+
+            int count = _blocksInARowNum; //count in for loop
+            for (int i = 0; i < count; i++)
+            {
+                Debug.Log("Unassigned " + unassignedTileNum);
+                Debug.Log("Block in a row " + blocksInARowNum);
+                int tempBlockLength;
+                if (i != count - 1 && blocksInARowNum != 1)
+                {
+                    tempBlockLength = Random.Range(1, unassignedTileNum - blocksInARowNum + 1);
+                    while (tempBlockLength >= 5)
+                    {
+                        tempBlockLength = Random.Range(1, unassignedTileNum - blocksInARowNum + 1);
+                    }
+                }
+                else
+                {
+                    tempBlockLength = unassignedTileNum;
+                }
+
+                Debug.Log("Temp Block: " + tempBlockLength);
+                for (int j = 1; j <= tempBlockLength; j++)
+                {
+                    AddToLine(tempBlockLength * 10 + j);
+                }
+
+                unassignedTileNum -= tempBlockLength;
+                blocksInARowNum--;
+            }
+        }
+
+        void AddToLine(int _value)
+        {
+            for (int i = 0; i < lineValue.Length; i++)
+            {
+                if (lineValue[i] == -1)
+                {
+                    lineValue[i] = _value;
+                    Debug.Log("Add " + _value + " at " + i);
+                    DebugLogArray(lineValue);
+                    break;
+                }
+            }
+        }
+
+        DebugLogArray(lineValue);
+        Debug.Log("END---------------------------------------------------------------------");
+    }
+
+    private void DebugLogArray(int[] array)
+    {
+        string output = "";
+        for (int i = 0; i < array.Length; i++)
+        {
+            output += array[i] + " ";
+        }
+
+        Debug.Log(output);
     }
 
     #endregion
